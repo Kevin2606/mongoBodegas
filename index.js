f9c086e833cbb8da1737c5412a2c7e801c8d7cf6
Kevin Gallardo <andreskevin2606@gmail.com>
2023-08-16 17:15:05 -0500

    fix: :sparkles: Se añadio endpoints de productos

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bodegasRouter from './routers/bodegasRouter.js';
+import productosRouter from './routers/productosRouter.js';
 
 dotenv.config();
 
 const app = express();
 
-app.use('/', bodegasRouter);
+app
+.use('/bodegas', bodegasRouter)
+.use('/productos', productosRouter)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening on http://localhost:${process.env.PORT}`);
